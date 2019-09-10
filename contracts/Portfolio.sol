0e4bbc93cb4f67d264389a0c9fd237d589c282b6
Programalyst <leonard.lin.2003@gmail.com>
2019-09-10 17:50:03 +0800

    Updated Portfolio.sol syntax to Solidity v5+ Added constructor and add asset function

--- contracts/Portfolio.sol
+++ contracts/Portfolio.sol
@@ -1,6 +1,10 @@
 pragma solidity 0.5.10;
 
 contract Portfolio {
+    //portfolio variables
+    address ownerAddress; //Ethereum address
+    bytes32 ownerName;
+
     // Model an Asset
     struct Asset {
         uint id;
@@ -9,11 +13,26 @@ contract Portfolio {
     }
 
     // Store Assets
-    // Fetch Asset
-    mapping(uint => Asset) public candidates;
+
+    // Fetch list of Assets
+    mapping(uint => Asset) public assets; //this is an key-value pair array that maps the id to the Asset structs
+
     // Store Assets Count
+    //Need to store the asset/transaction count as there is no way to find the length of the mapping array or iterate through it
     uint public assetsCount;
 
+    //constructors are optional in this version of Solidity
+    //constructor function is not deployed to the blockchain
+    constructor (address _ownerAddress, bytes32 _ownerName) public {
+        ownerAddress = _ownerAddress;
+        ownerName = _ownerName;
+    }
 
-
+    //Add Assets. underscore to indicate local variable
+    //storage saves the name string to contract storage and it can persist between functions
+    //memory is cheaper
+    function addAsset (string memory _name, uint _numUnits) private {
+        assetsCount++;
+        assets[assetsCount] = Asset(assetsCount, _name, _numUnits);
+    }
 }
