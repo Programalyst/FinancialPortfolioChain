0e68fa5298141f5997478162b807e54f0396dec8
Leonard <leonard@gamenami.com>
2019-09-13 16:28:47 +0800

    Installed and added Web3 to connect to ETH blockchain. Added web3Provider and web3 vars to connect to ETH blockchain

--- app.js
+++ app.js
@@ -16,5 +16,31 @@ app.use('/api/users', require('./routes/api/users'));
 
 const PORT = process.env.PORT || 5000;
 
+//connect to the blockchain
+const Web3 = require('web3'); // import web3 v1.0 constructor
+
+    if(typeof web3 !== 'undefined'){
+        //if a web3 instance is already provided by Meta Mask
+        web3Provider = web3.currentProvider;
+        web3 = new Web3(web3.currentProvider);
+    } else {
+        // Specifiy default instance if no web3 instance provided
+        web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+        web3 = new Web3(web3Provider);
+    }
+/*
+    
+    // Instantiate a new truffle contract from the artifact
+    contracts.Portfolio = TruffleContract(portfolio);
+    // Connect provider to interact with contract
+    contracts.Portfolio.setProvider(web3Provider);
+  
+*/
+    // Load account data
+    web3.eth.getCoinbase(function(err, account) {
+        if (err === null) {
+          console.log("Account = " + account);
+        }
+    });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
